f243acbf258ff7b4bb9090bdc768322b7a92f0e7
s07150853.gdmec.edu.cn <122714741@qq.com>
2016-10-30 19:42:41 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/s07150853/intent/IntentDemo3.java
@@ -0,0 +1,16 @@
+package cn.edu.gdmec.s07150853.intent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2016/10/30 0030.
+ */
+public class IntentDemo3 extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent_demo3);
+    }
+}
